47dec76e8b483d2ef28fc066f80f6929d50bd43f
Canon11 <mrt014kzm@gmail.com>
2019-03-29 17:31:08 +0900

    implement category list

--- src/components/CategoryList.vue
+++ src/components/CategoryList.vue
@@ -1,11 +1,35 @@
 <template>
   <div class="category-list">
+    <div>
+      <router-link to="/">back to top</router-link>
+    </div>
     <h1>CategoryList</h1>
+    <div v-for="category in categories" :key="category.id">
+      <p>ID: {{ category.id }}</p>
+      <p>Name: {{ category.name }}</p>
+    </div>
   </div>
 </template>
 
 <script>
+import ApiHost from "../api/ApiHost";
+import axios from "axios";
+
 export default {
-  name: "CategoryList"
+  name: "CategoryList",
+  data: function() {
+    return {
+      categories: {}
+    };
+  },
+  created: function() {
+    axios
+      .get(ApiHost.url + "/categories", {
+        headers: { "Content-Type": "application/json" }
+      })
+      .then(response => {
+        this.categories = response.data;
+      });
+  }
 };
 </script>
