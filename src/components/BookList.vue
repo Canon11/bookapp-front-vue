aeb588a83ba03fa848028403a1d2dd0eb5dd916d
Canon11 <mrt014kzm@gmail.com>
2019-03-29 17:27:39 +0900

    implement api function in book list

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="book-list">
+    <div>
+      <router-link to="/">back to top</router-link>
+    </div>
     <h1>BookList</h1>
+    <div v-for="book in books" :key="book.id">
+      <p>Name: {{ book.name }}</p>
+      <p>Category: {{ categories[book.category] ? categories[book.category] : 'undefined' }}</p>
+    </div>
   </div>
 </template>
 
@@ -10,13 +17,30 @@ import axios from "axios";
 
 export default {
   name: "BookList",
+  data: function() {
+    return {
+      books: [],
+      categories: {}
+    };
+  },
   created: function() {
     axios
       .get(ApiHost.url + "/books", {
         headers: { "Content-Type": "application/json" }
       })
       .then(response => {
-        console.log(response);
+        this.books = response.data;
+      });
+    axios
+      .get(ApiHost.url + "/categories", {
+        headers: { "Content-Type": "application/json" }
+      })
+      .then(response => {
+        let categories = response.data;
+        for (var i in categories) {
+          var category = categories[i];
+          this.categories[category.id] = category.name;
+        }
       });
   }
 };
